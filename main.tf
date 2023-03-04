b305839a2551877b9c6ed96c10a2f393a0be252a
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-03-05 01:10:19 +0900

    .gitignoreと変数を追加

--- main.tf
+++ main.tf
@@ -7,8 +7,40 @@ terraform {
   }
 }
 
+variable "pm_api_token_id" {
+  type = string
+  description = "Proxmox API token ID"
+}
+
+variable "pm_api_token_secret" {
+  type = string
+  sensitive = true
+  description = "Proxmox API token secret"
+}
+
+variable "pm_api_url" {
+  type = string
+  sensitive = true
+  description = "Proxmox URL"
+}
+
+variable "pm_ssh_public_key" {
+  type = string
+  sensitive = true
+  description = "SSH public key"
+}
+
+variable "pm_ssh_private_key" {
+  type = string
+  sensitive = true
+  description = "SSH private key"
+}
+
 provider "proxmox" {
-  pm_tls_insecure = true
+  pm_tls_insecure = false
+  pm_api_url = var.pm_api_url
+  pm_api_token_id = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
 }
 
 
@@ -36,8 +68,7 @@ resource "proxmox_vm_qemu" "cf-tunnel" {
   cipassword = "debian"
   ssh_user = "debian"
 
-  sshkeys = <<EOF
-EOF
-
+  sshkeys = var.pm_ssh_public_key
+  
+  ssh_private_key = var.pm_ssh_private_key
 }
-
