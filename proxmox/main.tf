72838c9ad685d5e2c8191e75f50cbc0706279f0e
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-04-03 12:43:05 +0900

    add debug outputs

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -49,6 +49,11 @@ provider "proxmox" {
   pm_api_url          = var.pm_api_url
   pm_api_token_id     = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
+
+  pm_debug = true
+  pm_log_levels = {
+    _default = "debug"
+  }
 }
 
 
