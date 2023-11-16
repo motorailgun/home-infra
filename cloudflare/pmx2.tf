fb71e802572df9a777e6e510d6763faf0c63bc3b
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-11-16 15:52:35 +0900

    cloudflare: pmx2: fix

--- cloudflare/pmx2.tf
+++ cloudflare/pmx2.tf
@@ -1,62 +1,11 @@
-terraform {
-  cloud {
-    organization = "motorailgun"
-
-    workspaces {
-      name = "home-infra-cloudflare"
-    }
-  }
-
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.2.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-}
-
-variable "cloudflare_token" {
-  description = "Cloudflare API token created at https://dash.cloudflare.com/profile/api-tokens"
-  type        = string
-  sensitive   = true
-}
-
-variable "cloudflare_account_id" {
-  description = "Account ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "cloudflare_zone_id" {
-  description = "Zone ID for yukari.uk"
-  type        = string
-  sensitive   = true
-}
-
-variable "cloudflare_idp_github_id" {
-  description = "ID of IdP of GitHub Auth"
-  type        = string
-  sensitive   = true
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_token
-}
-
-provider "random" {
-}
-
-resource "random_id" "tunnel_secret" {
+resource "random_id" "tunnel_secret_pmx2" {
   byte_length = 35
 }
 
 resource "cloudflare_tunnel" "pmx2" {
   account_id = var.cloudflare_account_id
   name       = "pmx2"
-  secret     = random_id.tunnel_secret.b64_std
+  secret     = random_id.tunnel_secret_pmx2.b64_std
 }
 
 resource "cloudflare_tunnel_config" "pmx2" {
@@ -72,7 +21,7 @@ resource "cloudflare_tunnel_config" "pmx2" {
 resource "cloudflare_record" "pmx2" {
   zone_id = var.cloudflare_zone_id
   name    = "pmx2"
-  value   = cloudflare_tunnel..cname
+  value   = cloudflare_tunnel.cname
   type    = "CNAME"
   proxied = true
 }
@@ -87,7 +36,7 @@ resource "cloudflare_access_application" "pmx2" {
   auto_redirect_to_identity = true
 }
 
-resource "cloudflare_access_policy" "github" {
+resource "cloudflare_access_policy" "github_pmx2" {
   application_id = cloudflare_access_application.coder.id
   zone_id        = var.cloudflare_zone_id
   name           = "policy for pmx2/allow github auth"
