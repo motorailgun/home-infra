0c612fe6d4974dc743628bfc38c7949a99c3df60
motorailgun <28751910+motorailgun@users.noreply.github.com>
2025-12-05 18:38:42 +0900

    raspberry_pi: fix init script

--- raspberry_pi/init.sh
+++ raspberry_pi/init.sh
@@ -5,20 +5,19 @@ SERVER=172.17.1.2
 
 MAC=$(cat /sys/class/net/eth0/address | tr ':' '-')
 
-if ! mount -t nfs -o vers=4 $SERVER:/srv/$MAC /mnt/check 2>/dev/null; then
+if  mount -t nfs -o vers=4 $SERVER:/srv/$MAC /mnt 2>/dev/null; then
+    umount /mnt
+else
     mount -t nfs -o vers=4 $SERVER:/srv/ /mnt
 
     cp -av /mnt/rpi-template /mnt/$MAC
 
     echo "proc                  /proc   proc    defaults        0 0" > /mnt/$MAC/etc/fstab
-    echo "172.17.1.2:/srv/$MAC  /       nfs     rw,vers=4      0 0" >> /mnt/$MAC/etc/fstab
+    echo "$SERVER:/srv/$MAC  /       nfs     rw,vers=4      0 0" >> /mnt/$MAC/etc/fstab
 
     umount /mnt
-else
-    umount /mnt/check
 fi
 
 mount -t nfs -o rw,vers=4 $SERVER:/srv/$MAC /mnt/
 
-
-exec switch_root /mnt/merged /sbin/init
+exec switch_root /mnt /sbin/init
